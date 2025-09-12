230bb423bdaee96a0fdc313ac8f16fbbfe5b5ab2
ffinelli <ffinelli@kainet.dev>
2025-09-12 15:14:27 -0300

    creando migraciones nuevamente y realizando cambios para conectar backend con el frontend

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import userRoutes from './routes/user.routes';
 import promotionRoutes from './routes/promotion.routes';
 import providerRoutes from './routes/provider.routes';
 import authRoutes from './routes/auth.routes';
+import cors from 'cors';
 
 const main = async () => {
 
@@ -15,24 +16,31 @@ const main = async () => {
 
   const orm = await MikroORM.init(mikroOrmConfig);
 
-
+  // ⚡ Cors debe ir antes de las rutas
+  app.use(cors({
+    origin: 'http://localhost:5173', // origen de tu front
+    methods: ['GET','POST','PUT','DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  }));
+  
   // Middleware para manejar RequestContext
   app.use((req, res, next) => {
     RequestContext.create(orm.em, next);
   });
 
   // Rutas de autenticación (debe ir después del RequestContext)
-  app.use('/auth', authRoutes);
+  const apiRouter = express.Router();
 
-  app.use('/products', productRoutes);
+  apiRouter.use('/auth', authRoutes);
+  apiRouter.use('/products', productRoutes);
+  apiRouter.use('/ingredients', ingredientRoutes);
+  apiRouter.use('/users', userRoutes);
+  apiRouter.use('/promotions', promotionRoutes);
+  apiRouter.use('/providers', providerRoutes);
 
-  app.use('/ingredients', ingredientRoutes);
+  app.use('/api', apiRouter);
 
-  app.use('/users', userRoutes);
-
-  app.use('/promotions', promotionRoutes);
-
-  app.use('/providers', providerRoutes);
 
   app.listen(3000, () => console.log('Server running on port 3000'));
 };
